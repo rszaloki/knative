c86d300e8ba0c46094a5a4a8a598193cdef2566b
Robert Szaloki <r.szaloki@gmail.com>
2021-01-28 10:17:05 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 
 app.use((ctx) => {
-    ctx.response.body = "Hello Deno!!!!";
+    ctx.response.body = "Hello Deno World!!";
 });
 
 await app.listen({ port: 8080 });
